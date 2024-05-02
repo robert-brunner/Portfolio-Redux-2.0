0459e32c877fedd1907ec0527ffa0bd582aaa029
robert-brunner <brunner.robert.work@gmail.com>
2024-05-01 21:39:38 -0400

    Hide add hide

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,8 +9,6 @@ import { FiAward, FiUsers } from "react-icons/fi";
 import { ImFolderPlus } from "react-icons/im";
 
 const About = () => {
-
-
   useEffect(() => {
     AOS.init({
       duration: 1200,
@@ -19,73 +17,43 @@ const About = () => {
 
   return (
     <section className='about_section' data-aos="fade-up-left" id='about'>
-      
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
       <div className="container about_container">
-        
         <div>
-
-          <div  className="about_me">
-
-            <img 
-            className='about_me_image' src={Selfie} alt="AboutPicture" />
-
+          <div className="about_me">
+            <img className='about_me_image' src={Selfie} alt="AboutPicture" />
           </div>
-
         </div>
 
-          <div className="about_content">
-
-            <div className="about_cards">
-
+        <div className="about_content">
+          <div className="about_cards">
             <article className='about_card'>
-
               <FiAward className='about_icon'/>
-
               <h5>Experience</h5>
-
               <small>1 Year</small>
-
             </article>
             
-
-            <article className='about_card'>
-
+            {/* Comment out or remove this section to hide the Clients card */}
+            {/* <article className='about_card'>
               <FiUsers className='about_icon'/>
-
-              {/* <h5>Clients</h5>
-
-              <small>5+ Worldwide</small> */}
-
-            </article>
+              <h5>Clients</h5>
+              <small>2 </small>
+            </article> */}
             
-
             <article className='about_card'>
-
               <ImFolderPlus className='about_icon'/>
-
               <h5>Projects</h5>
-
-              <small>3+ Completed</small>
-
+              <small>10+ Completed</small>
             </article>
-
-            </div>
-
-            <p>
-
-            Enthusiastic developer with a versatile background in multimedia, visual editing, and home security and automation.
-
-            </p>
-
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-
           </div>
-
+          <p>
+            Enthusiastic developer with a versatile background in multimedia, visual editing, and home security and automation.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
       </div>
-
     </section>
   )
 }

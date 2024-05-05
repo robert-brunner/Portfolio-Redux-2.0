d016df4bf9c3282deb21b9de27f682f087f5f09a
robert-brunner <brunner.robert.work@gmail.com>
2024-05-04 23:19:26 -0400

    addded buttons to portfolio to make it less buggy

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,29 +1,23 @@
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
 import React, { useEffect, useState } from "react";
-// Import Swiper React components
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
+import SwiperCore, { EffectCoverflow, Pagination, Navigation } from "swiper/core";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import SwiperCore, { EffectCoverflow, Pagination } from "swiper/core";
-// import './styles.css';
-// import IMG1 from '../../assets/P1-E-Commerce.png';
+import { portfolio } from "../../constants";
 import "./Portfolio.css";
-
-import {portfolio} from "../../constants";
 
 const Portfolio = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 720);
+      setIsMobile(window.innerWidth <= 600); // Check for mobile size
     };
 
     handleResize(); // Initial check
@@ -35,7 +29,8 @@ const Portfolio = () => {
     AOS.init({ duration: 1500 });
   }, []);
 
-  SwiperCore.use([Pagination]);
+  // Adding Navigation to SwiperCore
+  SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
 
   return (
     <section id="portfolio">
@@ -45,7 +40,8 @@ const Portfolio = () => {
       <Swiper
         data-aos="fade-up"
         effect={"coverflow"}
-        grabCursor={true}
+        grabCursor={!isMobile} // Disable grabCursor on mobile
+        allowTouchMove={!isMobile} // Disable touch move on mobile
         centeredSlides={true}
         slidesPerView={"auto"}
         coverflowEffect={{
@@ -55,53 +51,42 @@ const Portfolio = () => {
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={{ clickable: true }}
-        modules={[EffectCoverflow, Pagination]}
+        pagination={{
+          clickable: !isMobile // Make pagination non-clickable on mobile
+        }}
+        navigation={isMobile ? { // Enable navigation only on mobile
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        } : false}
+        modules={[EffectCoverflow, Pagination, Navigation]}
       >
         {portfolio.map(({ id, gif, title, github, demo, tag }) => (
-          <SwiperSlide
-            style={{
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              width: isMobile ? '56vh' : '70vh',
-              margin: isMobile ? '0px' : 'auto',
-              height: '60vh'
-            }}
-            key={id}
-          >
+          <SwiperSlide key={id} style={{
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            width: isMobile ? '56vh' : '70vh',
+            margin: isMobile ? '0px' : 'auto',
+            height: '60vh'
+          }} className={isMobile ? "disable-interaction" : ""}>
             <div className="portfolio_item_image">
               <img height={190} src={gif} alt={title} />
               <div className="portfolio_item_content">
                 <h3 style={{ textAlign: "center" }}>{title}</h3>
                 <div className="portfolio_item_cta">
-                  {github && (
-                    <a
-                      href={github}
-                      rel="noreferrer"
-                      className="btn"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  )}
-                  {/* Uncomment and use the below if demo links are also conditional */}
-                  {demo && (
-                    <a
-                      href={demo}
-                      rel="noreferrer"
-                      className="btn btn-primary"
-                      target="_blank"
-                    >
-                      Demo
-                    </a>
-                  )}
-                 
+                  {github && <a href={github} className="btn" target="_blank" rel="noreferrer">Github</a>}
+                  {demo && <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Demo</a>}
                 </div>
                 <p className="tag_css">{tag}</p>
               </div>
             </div>
           </SwiperSlide>
         ))}
+        {isMobile && (
+          <>
+            <div className="swiper-button-next"></div>
+            <div className="swiper-button-prev"></div>
+          </>
+        )}
       </Swiper>
     </section>
   );
